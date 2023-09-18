00bd991594df45506122233e8643d83a9eb86116
lonexd3vil <itzlonewolfzproductions@gmail.com>
2023-09-19 00:43:01 +0530

    fixed home function typo

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view(['GET'])
 def baseHome(request):
-    pass
+    return Response("HE<LLO)SDLOOOOOOOOO")
